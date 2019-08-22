756b77cc40a0a06d6c0c2b6f8bb508496154a451
benyamin <benyamin.aberkane@hotmail.fr>
2019-08-22 20:08:23 +0200

    'message'

---
+++ src/app/post-list-component/post-list-component.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-component',
+  templateUrl: './post-list-component.component.html',
+  styleUrls: ['./post-list-component.component.scss']
+})
+export class PostListComponentComponent implements OnInit {
+
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postLoveIts: number;
+  @Input() postDate: Date;
+
+  @Input() myPosts;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item-component',
+  templateUrl: './post-list-item-component.component.html',
+  styleUrls: ['./post-list-item-component.component.scss']
+})
+export class PostListItemComponentComponent implements OnInit {
+
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postLoveIts: number;
+  @Input() postDate: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getColor() {
+  }
+
+  onDontLoveIt() {
+    this.postLoveIts--;
+  }
+
+  onLoveIt() {
+    this.postLoveIts++;
+  }
+
+}
